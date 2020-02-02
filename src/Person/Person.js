2d62507e3277e1042d11f2e0fc03fd7b34d0a063
RaidenDilan <raiden18@me.com>
2020-02-02 22:31:23 +0000

    Outputting Dynamic Content

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,7 +1,8 @@
 import React from 'react';
 
 const person = () => {
-  return <p>I'm a Person!</p>
-}
+  // We can execute one line expressions, short simple expressions, like calculations or function calls here.
+  return <p>I'm a Person and I'm { Math.floor(Math.random() * 30) } years old! </p>
+};
 
 export default person;
