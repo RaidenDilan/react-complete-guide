bb50803933f15e974213dae8eceb15dc72bb8700
RaidenDilan <raiden18@me.com>
2020-02-03 01:49:39 +0000

    Passing Method References Between Components

--- src/App.js
+++ src/App.js
@@ -1,53 +1,67 @@
-import React, { useState } from 'react';
+import React, { Component } from 'react';
 import './App.css';
 import Person from './Person/Person';
 
-const app = props => {
-  // useState return an array with exactly two elemens and always two elements
-  // the first element we get back will always be our current state.
-  // the second element we get back will always be a functuon that allows us to update this state.
-  // Such that React is aware of it and will renrender thsi component.
-  const [personsState, setPersonsState] = useState({
+class App extends Component {
+  // 'this. keyword' referes to the class
+  // use state with care.
+  state = {
     persons: [
       { name: 'Max', age: 26 },
       { name: 'Manu', age: 29 },
       { name: 'Stephanie', age: 24 }
-    ]
-  });
+    ],
+    // otherState = 'some other value'
+  }
 
-  // You can use useState as many time as you want.
-  // useState can even pass a String, Object or a Number, Array or a Bollean.
-  const [ otherState, setOtherState ] = useState('some other value');
-
-  console.log(personsState, otherState);
-
-  // We can have functions inside functions
-  const switchNameHandler = () => {
+  // METHODS
+  switchNameHandler = (newName) => {
     // console.log('Was clicked!');
     // DON'T DO THIS: this.state.persons[0].name = 'Raiden';
-    setPersonsState({
+    this.setState({
       persons: [
-        { name: 'Raiden', age: 26 },
+        { name: newName, age: 26 },
         { name: 'Manu', age: 29 },
         { name: 'Stephanie', age: 20 }
       ]
-      // otherState: personsState.otherState // THIS: is not a good approach to manually include all old state data or untouched state.
-    });
+    })
   };
 
-  // this. keyword only exists in class based react components and we have no such class based react componenets.
-  // personsState. keyword because we are using a functional component.
-  return (
-    <div className="App">
-      <h1>Hi I'm a React App</h1>
-      <p>This is really working!</p>
-      <button onClick={ switchNameHandler }>Switch Name</button>
-      <Person name={ personsState.persons[0].name } age={ personsState.persons[0].age }/>
-      <Person name={ personsState.persons[1].name } age={ personsState.persons[1].age }>My Hobbies: Racing</Person>
-      <Person name={ personsState.persons[2].name } age={ personsState.persons[2].age }/>
-    </div>
-  );
-  // return React.createElement('div', { className: 'App' }, React.createElement('h1', null, 'Does this work now?'));
+  // We can pass a value to switchNameHandler like newName value
+  // How do we pass that new data
+  // This are two ways of doing this
+  // With .bind(this, 'list of arguements')
+  // OR
+  // With new syntax which is ArrowFunction
+  // When using ArrowFunction implicitly returns the body like so () => return this.switchNameHandler()
+  // We are also passing an anonymise function when using this.switchNameHandler() - So it does not get executed immediatlay even when using switchNameHandler is invoked
+  // This syntax - () => this.switchNameHandler('Raiden!!') can be inefficient.
+
+  render() {
+    return (
+      <div className="App">
+        <h1>Hi I'm a React App</h1>
+        <p>This is really working!</p>
+        <button onClick={ () => this.switchNameHandler('Raiden!!') }>Switch Name</button>
+        <Person
+          name={ this.state.persons[0].name }
+          age={ this.state.persons[0].age }
+          />
+        <Person
+          name={ this.state.persons[1].name }
+          age={ this.state.persons[1].age }
+          click={ this.switchNameHandler.bind(this, 'Max') }
+          >
+          My Hobbies: Racing
+        </Person>
+        <Person
+          name={ this.state.persons[2].name }
+          age={ this.state.persons[2].age }
+          />
+      </div>
+    );
+    // return React.createElement('div', { className: 'App' }, React.createElement('h1', null, 'Does this work now?'));
+  }
 }
 
-export default app;
+export default App;
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,10 +1,14 @@
 import React from 'react';
 
+// This is a Stateless Component because it has no internal state management
+// It is good practice to use as many of this Stateless Component then StateFull Component
+// Stateless Components are also called Presentational Components because they present something.
+
 // This is a reusable Component
 const person = (props) => {
   return (
     <div>
-      <p>I'm { props.name } and I am { props.age } years old! </p>
+      <p onClick={ props.click }>I'm { props.name } and I am { props.age } years old! </p>
       <p>{ props.children }</p>
     </div>
   )
