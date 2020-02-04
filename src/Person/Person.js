08198638e2c369964fb1599d27e28eb1f87dc77e
RaidenDilan <raiden18@me.com>
2020-02-04 22:45:36 +0000

    CSS Modules & Media Queries

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,38 +1,19 @@
 import React from 'react';
-// import Radium from 'radium';
-import styled from 'styled-components'
+import classes from './Person.css';
 
-import './Person.css';
+/* For React version 2.x or higher rename Person.css to Person.module.css
+ * Then you don't need to run npm run eject to use CSS Modules
+ * Then Import like this ---> import classes from './Person.module.css';
+ */
 
 // This is a Stateless Component because it has no internal state management
 // It is good practice to use as many of this Stateless Component then StateFull Component
 // Stateless Components are also called Presentational Components because they present something.
 
-// This a function Component
-const StyledDiv = styled.div`
-  width: 60%;
-  margin: 16px auto;
-  border: 1px solid #eee;
-  box-shadow: 0 2px 3px #ccc;
-  padding: 16px;
-  text-align: center;
-
-  @media (min-width: 500px) {
-    width: 450px;
-  }
-`;
-
 // This is a reusable Component
 const person = (props) => {
-  // const style = {
-  //   '@media (min-width: 500px)': {
-  //     width: '450px'
-  //   }
-  // };
-
   return (
-    // <div className="Person" style={ style }>
-    <StyledDiv>
+    <div className={ classes.Person }>
       <p onClick={ props.click }>I'm { props.name } and I am { props.age } years old! </p>
       <p>{ props.children }</p>
       <input
@@ -40,9 +21,8 @@ const person = (props) => {
         onChange={ props.changed }
         value={ props.name }
         />
-    </StyledDiv>
+    </div>
   )
 };
 
 export default person;
-// export default Radium(person);
