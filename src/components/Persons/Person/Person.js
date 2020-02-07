d0d6a7e8c3db980fe59a7837055f964ca8b3c6e2
RaidenDilan <raiden18@me.com>
2020-02-07 23:29:39 +0000

    Using React.Fragment

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
+// import React, { Component, Fragment } from 'react';
 import classes from './Person.css';
-
 import Aux from '../../../hoc/Aux';
 
 /* For React version 2.x or higher rename Person.css to Person.module.css
@@ -17,6 +17,8 @@ class Person extends Component {
   render() {
     console.log('[Person.js] rendering...');
 
+    // React has a it's own Aux Higher Order Component called React.Fragment.
+    // You can also import Fragment from react. and replace Aux with Fragment.
     return (
       <Aux>
         <p onClick={ this.props.click }>I'm { this.props.name } and I am { this.props.age } years old! </p>
